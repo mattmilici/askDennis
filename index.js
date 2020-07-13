a7a245c11c5d30f1f3382260aba56a1e9f506e19
Matthew Milici <milici.m915@gmail.com>
2020-07-13 07:01:39 -0700

    added sound

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 function myFunction() {
+    // changes response
     let random = ["No shot! Go eat a vegetable...", "Are you serious? Look at your bank account?", "That's going to be a no from me!", "lol... no", "No", "Nah", "Maybe get a job first?"];
     let randomResponse = Math.floor(Math.random() * random.length);
     document.getElementById('test').innerHTML = random[randomResponse];
+    // adds sound
+    var audio = new Audio('./sounds/fart1.wav');
+    audio.play();
     return;
 }
-
-// function play() {
-//     var audio = document.getElementById("audio");
-//     audio.play();
-// }
