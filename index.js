6ede93de881b44ffa4dc6c9b710ba4e749b2959b
Matthew Milici <milici.m915@gmail.com>
2020-07-13 06:22:09 -0700

    image fix

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function myFunction() {
     let random = ["No shot! Go eat a vegetable...", "Are you serious? Look at your bank account?", "That's going to be a no from me!", "lol... no", "No", "Nah", "Maybe get a job first?"];
     let randomResponse = Math.floor(Math.random() * random.length);
-    document.getElementById("test").innerHTML = random[randomResponse];
+    document.getElementById('test').innerHTML = random[randomResponse];
+    return;
 }
 
-function play() {
-    var audio = document.getElementById("audio");
-    audio.play();
-}
+// function play() {
+//     var audio = document.getElementById("audio");
+//     audio.play();
+// }
