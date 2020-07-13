9cdf20faa69d72547fe31ce6c80b895dee21a97b
Matthew Milici <milici.m915@gmail.com>
2020-07-12 19:52:10 -0700

    header changes

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function myFunction() {
-    let random = ["No shot! Go eat a vegetable...", "Are you serious? Look at your bank accoumnt?", "That's going to be a no from me!", "lol... no", "No", "Nah", "Maybe get a job first?"];
+    let random = ["No shot! Go eat a vegetable...", "Are you serious? Look at your bank account?", "That's going to be a no from me!", "lol... no", "No", "Nah", "Maybe get a job first?"];
     let randomResponse = Math.floor(Math.random() * random.length);
     document.getElementById("test").innerHTML = random[randomResponse];
+}
+
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
 }
