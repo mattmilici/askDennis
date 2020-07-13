22b37bc19ed7b909a6c2cc4400a35afed259926c
Matthew Milici <milici.m915@gmail.com>
2020-07-12 17:35:06 -0700

    changed background

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    let random = ["No shot! Go eat some vegetable...", "Are you serious? Have you seen your savings accoumnt?", "That's going to be a no from me!", "lol... no", "No", "Nah"];
+    let random = ["No shot! Go eat a vegetable...", "Are you serious? Look at your bank accoumnt?", "That's going to be a no from me!", "lol... no", "No", "Nah", "Maybe get a job first?"];
     let randomResponse = Math.floor(Math.random() * random.length);
     document.getElementById("test").innerHTML = random[randomResponse];
 }
