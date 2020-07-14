f4b62a8b8cdc7534b445256b949357c111c071d7
Matthew Milici <milici.m915@gmail.com>
2020-07-13 18:49:19 -0700

    added question

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function myFunction() {
     let random = ["No shot! Go eat a vegetable...", "Are you serious? Look at your bank account?", "That's going to be a no from me!", "lol... no", "No", "Nah", "Maybe get a job first?", "You know what... Yes, you deserve it!"];
     let randomResponse = Math.floor(Math.random() * random.length);
     document.getElementById('test').innerHTML = random[randomResponse];
-    // adds sound
 
+    // adds sound
     if (random[randomResponse] === random[random.length - 1]) {
         var audio = new Audio('./sounds/youwin.mp3');
         audio.play();
