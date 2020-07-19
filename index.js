651d23ffb98a9e2b1a00e98c65b8d53e73d7420e
Matthew Milici <milici.m915@gmail.com>
2020-07-18 17:42:33 -0700

    added night mode

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 function myFunction() {
     // changes response
-    let random = ["No shot! Go eat a vegetable...", "Are you serious? Look at your bank account?", "That's going to be a no from me!", "lol... no", "No", "Nah", "Maybe get a job first?", "You know what... Yes, you deserve it!"];
+    let random = [
+        "No shot! Go eat a vegetable...",
+        "Are you serious? Look at your bank account?",
+        "That's going to be a no from me!",
+        "lol... no",
+        "No",
+        "Nah",
+        "Maybe get a job first?",
+        "You know what... Yes, you deserve it!",
+    ];
     let randomResponse = Math.floor(Math.random() * random.length);
-    document.getElementById('test').innerHTML = random[randomResponse];
+    document.getElementById("test").innerHTML = random[randomResponse];
 
     // adds sound
     if (random[randomResponse] === random[random.length - 1]) {
-        var audio = new Audio('./sounds/youwin.mp3');
+        var audio = new Audio("./sounds/youwin.mp3");
         audio.play();
     } else {
-        var audio = new Audio('./sounds/fart1.wav');
+        var audio = new Audio("./sounds/fart1.wav");
         audio.play();
     }
     return;
 }
+
+// ---------------- nightmode --------------
+
+var container = document.querySelector("body");
+var mode = "body";
+
+function nightMode() {
+    if (mode === "dark") {
+        mode = "light";
+        container.setAttribute("class", "light");
+    } else {
+        mode = "dark";
+        container.setAttribute("class", "dark");
+    }
+}
+// ---------------- nightmode --------------
