2fe61d4d3b992f409d502fa10c0017e764d4ef99
Matthew Milici <milici.m915@gmail.com>
2020-07-18 19:51:18 -0700

    updates

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function myFunction() {
         "No",
         "Nah",
         "Maybe get a job first?",
-        "No shot! Go eat a vegetable...No shot! Go eat a vegetable...No shot! Go eat a vegetable...No shot! Go eat a vegetable...No shot! Go eat a vegetable...",
         "You know what... Yes, you deserve it!",
     ];
     let randomResponse = Math.floor(Math.random() * random.length);
@@ -34,13 +33,9 @@ function nightMode() {
     if (mode === "dark") {
         mode = "light";
         container.setAttribute("class", "light");
-        document
-            .querySelector("img")
-            .setAttribute("src", "images/dennis-photo.png");
     } else {
         mode = "dark";
         container.setAttribute("class", "dark");
-        document.querySelector("img").setAttribute("src", "images/robot-night.png");
     }
 }
 // ---------------- nightmode --------------
