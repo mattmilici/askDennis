92356eb366b4f418dcabe331a0715416f04c683b
Matthew Milici <milici.m915@gmail.com>
2020-08-03 08:10:21 -0700

    updated instructions again

---
+++ instructions.js
@@ -0,0 +1,14 @@
+$("#instructionsButton").on("click", instructions2);
+
+function instructions2() {
+    $("#instructionsHeader").text("Here's how it works");
+
+    $("#lead1").text("If your child is begging for a new toy");
+
+    $("#lead2").hide();
+    $("#lead3").hide();
+    $("#lead4").hide();
+    $("#lead5").hide();
+    $("#lead6").hide();
+    $("#lead7").hide();
+}
